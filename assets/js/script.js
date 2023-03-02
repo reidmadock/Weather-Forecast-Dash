343996378fa45750ef1efb952052c16c77d85072
Reid Madock <reidmadock@gmail.com>
2023-03-02 17:19:54 -0600

    added api call to get cooridinates of city

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,23 @@ var apiKeyTerm = "&appid=";
 var apiKey = "dacfab95576f6aec206ec9cde08bd9db";
 var unitsTerm = "&units=imperial"
 var testUrl = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=dacfab95576f6aec206ec9cde08bd9db"
+var latAndLon = new Array(); //latitude and longitude
+
+/* Trying to store lat and lon in individual variables caused issues with those variables getting reset
+after the function. So they're in an area, and to account for multiple searches, the array can be
+access by say [length - 2] and [length - 1] to get latitude and longitude respectively */
+function getCityCoords(inputCity) {
+    var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="
+    var limitTerm = "&limit=1"
+    var fullUrl = geoApiUrl + inputCity + limitTerm + apiKeyTerm + apiKey
+    fetch(fullUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (turkey) {
+        latAndLon.push(turkey[0].lat, turkey[0].lon)
+    });
+}
 
 function getApi(searchUrl) {
     fetch(searchUrl)
@@ -31,7 +48,9 @@ function loadHistory() {
 
 function citySearch (event) {
     event.preventDefault();
-    var searchedCity = inputCitySearch.value
+    var searchedCity = inputCitySearch.value;
+    getCityCoords(inputCitySearch.value);
+    console.log(latAndLon);
     var searchUrl = apiUrl + searchTerm + searchedCity + apiKeyTerm + apiKey + unitsTerm
     // console.log(searchUrl);
     localStorage.setItem(searchedCity, searchedCity);
