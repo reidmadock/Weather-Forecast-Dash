5c8b5f89831ac11b27450eee64de4a2d56038241
Reid Madock <reidmadock@gmail.com>
2023-03-04 16:18:56 -0600

    all information gets on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,122 +4,88 @@ var historyContainer = document.querySelector('#history-container');
 var todayForecast = document.querySelector('#today-forecast');
 var fiveDayForecast = document.querySelector('#five-day-forecast');
 
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
 var searchTerm = "q=";
 var apiKeyTerm = "&appid=";
 var apiKey = "dacfab95576f6aec206ec9cde08bd9db";
 var unitsTerm = "&units=imperial"
-var testUrl = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=dacfab95576f6aec206ec9cde08bd9db"
+var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?"
+var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="
+var limitTerm = "&limit=1"
 
+function Forecast(cityName, date, icon, temp, humid, wind) {
+    this.cityName = cityName;
+    this.date = date;
+    this.icon = icon;
+    this.temp = temp;
+    this.humid = humid;
+    this.wind = wind;
+}
 
-/* Trying to store lat and lon in individual variables caused issues with those variables getting reset
-after the function. So they're in an area, and to account for multiple searches, the array can be
-access by say [length - 2] and [length - 1] to get latitude and longitude respectively */
-function getCityCoords(inputCity) {
-    var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="
-    var limitTerm = "&limit=1"
-    var fullUrl = geoApiUrl + inputCity + limitTerm + apiKeyTerm + apiKey
-    var latAndLon = [41.8755616, -87.6244212]; //latitude and longitude
-    fetch(fullUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (turkey) {
-        latAndLon.push(turkey[0].lat, turkey[0].lon)
-    });
-    console.log(latAndLon)
+function renderForecast(cityName, date, icon, temp, humid, wind) {
+    var forecastCard = document.createElement('div');
+    forecastCard.className = "card";
+    var infoList = document.createElement('ul');
+    var dateEl = document.createElement('li');
+    var iconEl = document.createElement('li');
+    var tempEl = document.createElement('li');
+    var humidEl = document.createElement('li');
+    var windEl = document.createElement('li');
+    if (cityName.length > 0) {
+        var cityEl = document.createElement('li');
+        cityEl.textContent = cityName;
+        infoList.append(cityEl);
+    } 
+    dateEl.textContent = date;
+    iconEl.textContent = icon;
+    tempEl.textContent = temp;
+    humidEl.textContent = humid;
+    windEl.textContent = wind;
+    infoList.append(dateEl, iconEl, tempEl, humidEl, windEl);
+    forecastCard.append(infoList)
+    fiveDayForecast.append(forecastCard);
 }
 
 function getWeatherData(inputCity) {
-    var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="
-    var limitTerm = "&limit=1"
-    var fullUrl = geoApiUrl + inputCity + limitTerm + apiKeyTerm + apiKey
-    // var latAndLon = ["filler", "data"]; //latitude and longitude
-    // console.log("Before fetch: ", latAndLon);
-
-    fetch(fullUrl)
+    var fullGeoUrl = geoApiUrl + inputCity + limitTerm + apiKeyTerm + apiKey
+    
+    fetch(fullGeoUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (turkey) {
         return(turkey[0]);
     })
-    .then(function (data) {
-        var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?"
+    .then(function (data) {        
         var latTerm = "lat=" + data.lat;
         var lonTerm = "&lon=" + data.lon;
-        var fullFiveDayUrl = fiveDayUrl + latTerm + lonTerm + apiKeyTerm + apiKey;
+        var fullForecastUrl = forecastUrl + latTerm + lonTerm + apiKeyTerm + apiKey + unitsTerm;
 
-        // console.log("After fetch: ", latAndLon);
-        fetch(fullFiveDayUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (turkey) {
-            console.log(turkey);
-        });
+        asyncApiCall(fullForecastUrl);
     });
-
-    // var fiveDayUrl = "api.openweathermap.org/data/2.5/forecast?"
-    // var latTerm = "lat=" + latAndLon[0];
-    // var lonTerm = "&lon=" + latAndLon[1];
-    // var fullFiveDayUrl = fiveDayUrl + latTerm + lonTerm + apiKeyTerm + apiKey;
-
-    // console.log("After fetch: ", latAndLon);
-    // fetch(fullFiveDayUrl)
-    // .then(function (response) {
-    //     return response.json();
-    // })
-    // .then(function (turkey) {
-    //     console.log(turkey);
-    // });
 }
 
-function javaScriptIsObtuse(lat, lon) {
-    var fiveDayUrl = "api.openweathermap.org/data/2.5/forecast?"
-    var latTerm = "lat=" + lat;
-    var lonTerm = "&lon=" + lon;
-    var fullFiveDayUrl = fiveDayUrl + latTerm + lonTerm + apiKeyTerm + apiKey;
-
-    // console.log("After fetch: ", latAndLon);
-    fetch(fullFiveDayUrl)
-    .then(function (response) {
+async function asyncApiCall(url) {
+    const result = await fetch(url)
+    .then (function (response) {
         return response.json();
     })
-    .then(function (turkey) {
-        console.log(turkey);
-    });
+    var cityName = result.city.name 
+    console.log(cityName)
+    localStorage.setItem(cityName, cityName)
+    for (var i = 0; i < 6; i++) {
+        console.log(result.list[i])
+        var name = ""
+        if (i === 0) { name = result.city.name; }
+        renderForecast(
+            name,
+            result.list[i].dt,
+            result.list[i].weather[0].icon,
+            result.list[i].main.temp,
+            result.list[i].main.humidity,
+            result.list[i].wind.speed)
+    }
 }
 
-function getApiFiveDay() {
-    var fiveDayUrl = "api.openweathermap.org/data/2.5/forecast?"
-    console.log(latAndLon);
-    console.log("Lat: ", latAndLon[0])
-    console.log("Lon: ", latAndLon[1])
-    console.log(latAndLon);
-    var latTerm = "lat=" + latAndLon[latAndLon.length - 2];
-    var lonTerm = "&lon=" + latAndLon[latAndLon.length - 1];
-    var fullFiveDayUrl = fiveDayUrl + latTerm + lonTerm + apiKeyTerm + apiKey;
-    console.log(fullFiveDayUrl);
-    fetch(fullFiveDayUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (turkey) {
-        console.log(turkey);
-    });
-}
-/*
-function getApiToday(searchUrl) {
-    fetch(searchUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (turkey) {
-        console.log(turkey);
-    });
-}
-*/
 function loadFullHistory() {
     for (var i = 0; i < localStorage.length; i++) {
         var divKey = document.createElement('div');
@@ -130,23 +96,17 @@ function loadFullHistory() {
 
 function loadHistory() {
     var divKey = document.createElement('div');
-    divKey.textContent = localStorage.getItem(localStorage.key(localStorage.length - 1));
+    divKey.textContent = localStorage.getItem(localStorage.key(0));
     historyContainer.appendChild(divKey);
 }
 
 function citySearch (event) {
     event.preventDefault();
-    var searchedCity = inputCitySearch.value;
-    // getCityCoords(inputCitySearch.value);
+    // var searchedCity = inputCitySearch.value;
+    // if (searchedCity === localStorage.getItem(localStorage.key(0))) { return; }
     getWeatherData(inputCitySearch.value);
-    // console.log(latAndLon);
-    var searchUrl = apiUrl + searchTerm + searchedCity + apiKeyTerm + apiKey + unitsTerm
-    // console.log(searchUrl);
-    localStorage.setItem(searchedCity, searchedCity);
-    // console.log('starbucks');
-    // getApiToday(searchUrl); //Function call, function invocation
-    // getApiFiveDay();
-    loadHistory();
+    // localStorage.setItem(searchedCity, searchedCity);
+    // loadHistory();
 }
 
 loadFullHistory();
